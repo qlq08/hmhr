2d433e265628d114ac280f0bb522ac658978cb5c
权 <2071675988@qq.com>
2022-12-20 17:01:34 +0800

    完成了树形内容单独抽提成组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="departments-container">
     <div class="app-container">
-      <!-- 树形卡片布局 -->
+      <!-- 实现页面的基本布局 -->
       <el-card class="tree-card">
         <!-- 用了一个行列布局 -->
         <el-row
@@ -31,54 +31,26 @@
           </el-col>
         </el-row>
       </el-card>
-      <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
-      <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-        <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-        <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-        <el-row
-          slot-scope="{ data }"
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px; width: 100%"
-        >
-          <el-col>
-            <!-- 左侧内容 -->
-            <span>{{ data.name }}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>{{ data.manager }}</el-col>
-              <el-col>
-                <!-- 放置下拉菜单 -->
-                <el-dropdown>
-                  <!-- 内容 -->
-                  <span
-                    >操作
-                    <i class="el-icon-arrow-down" />
-                  </span>
-                  <!-- 具名插槽 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <!-- 下拉选项 -->
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                    <el-dropdown-item>编辑部门</el-dropdown-item>
-                    <el-dropdown-item>删除部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-
-            <!-- 右侧内容 -->
-          </el-col>
-        </el-row>
+      <!-- 用了一个行列布局 -->
+      <!-- 缺少treeNode -->
+      <treeToos :tree-node="company" :is-root="true" />
+      <el-tree :data="departs" :props="defaultProps" default-expand-all>
+        <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+        <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+        <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
+        <treeToos slot-scope="{ data }" :tree-node="data" />
       </el-tree>
     </div>
   </div>
 </template>
 
 <script>
+import treeToos from './components/tree-tools'
 export default {
   name: 'Departments',
+  components: {
+    treeToos
+  },
   data () {
     return {
       departs: [
@@ -92,7 +64,8 @@ export default {
         { name: '人事部', manager: '孙权' }],
       defaultProps: {
         label: 'name' // 表示 从这个属性显示内容
-      }
+      },
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' }
     }
   }
 }
