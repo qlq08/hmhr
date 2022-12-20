fa0f015a96f977d46885d2ba31480967093c0cad
权 <2071675988@qq.com>
2022-12-20 12:42:40 +0800

    设置菜单图标

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,15 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Layout from '@/layout'
+// 引入多个模块的规则
+
+import employeesRouter from './modules/employees'
+
+// 动态路由
+export const asyncRoutes = [
+  employeesRouter
+
+]
 
 export const constantRoutes = [
   {
@@ -31,8 +40,8 @@ export const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  scrollBehavior: () => ({ y: 0 }), // 管理滚动行为 如果出现滚动 切换就让 让页面回到顶部
+  routes: [...constantRoutes, ...asyncRoutes] // 临时合并所有的路由
 })
 
 const router = createRouter()
