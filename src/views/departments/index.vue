e4f6759e5ea758bfe7a6dfa0883f56bb7c3caf6f
权 <2071675988@qq.com>
2022-12-20 16:13:51 +0800

    完成了组织架构-头部布局的完成

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,13 +1,55 @@
 <template>
   <div class="departments-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <!-- 卡片组件（分为头部和身体） -->
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <template #header class="clearfix">
+          <!-- tab导航 -->
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="组织架构" name="first">组织架构</el-tab-pane>
+          </el-tabs>
+        </template>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Departments',
+  data () {
+    return {
+      activeName: 'first' // 被激活的 Tab 标签栏
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box-card {
+  padding: 10px 30px;
+}
+
+.tab-pane {
+  padding: 20px 80px;
+
+  .company {
+    margin-left: 12px;
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  .department-header {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+  }
+}
+</style>
