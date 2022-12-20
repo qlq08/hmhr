493f87dcf21648d93e8745244a78fbae836051ae
权 <2071675988@qq.com>
2022-12-20 16:44:51 +0800

    完成了树形的静态组织架构

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,16 +1,77 @@
 <template>
   <div class="departments-container">
     <div class="app-container">
-      <!-- 卡片组件（分为头部和身体） -->
-      <el-card class="box-card">
-        <!-- 头部 -->
-        <template #header class="clearfix">
-          <!-- tab导航 -->
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="组织架构" name="first">组织架构</el-tab-pane>
-          </el-tabs>
-        </template>
+      <!-- 树形卡片布局 -->
+      <el-card class="tree-card">
+        <!-- 用了一个行列布局 -->
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
+          <el-col>
+            <span>江苏传智播客教育科技股份有限公司</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <!-- 两个内容 -->
+              <el-col>负责人</el-col>
+              <el-col>
+                <!-- 下拉菜单 element -->
+                <el-dropdown>
+                  <span> 操作<i class="el-icon-arrow-down" /> </span>
+                  <!-- 下拉菜单 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
       </el-card>
+      <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+      <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
+        <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
+        <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
+        <el-row
+          slot-scope="{ data }"
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px; width: 100%"
+        >
+          <el-col>
+            <!-- 左侧内容 -->
+            <span>{{ data.name }}</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>{{ data.manager }}</el-col>
+              <el-col>
+                <!-- 放置下拉菜单 -->
+                <el-dropdown>
+                  <!-- 内容 -->
+                  <span
+                    >操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 具名插槽 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <!-- 下拉选项 -->
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                    <el-dropdown-item>编辑部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+
+            <!-- 右侧内容 -->
+          </el-col>
+        </el-row>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -20,36 +81,26 @@ export default {
   name: 'Departments',
   data () {
     return {
-      activeName: 'first' // 被激活的 Tab 标签栏
-    }
-  },
-  methods: {
-    handleClick (tab, event) {
-      console.log(tab, event)
+      departs: [
+        {
+          name: '总裁办', manager: '曹操',
+          children: [
+            { name: '董事会', manager: '曹丕' }
+          ]
+        },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }],
+      defaultProps: {
+        label: 'name' // 表示 从这个属性显示内容
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.box-card {
-  padding: 10px 30px;
-}
-
-.tab-pane {
-  padding: 20px 80px;
-
-  .company {
-    margin-left: 12px;
-    font-weight: bold;
-    font-size: 14px;
-  }
-
-  .department-header {
-    height: 50px;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #ccc;
-  }
+.tree-card {
+  padding: 30px 40px;
+  font-size: 14px;
 }
 </style>
